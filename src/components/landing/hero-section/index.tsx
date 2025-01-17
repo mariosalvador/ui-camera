a2666ee6baf858d109cd85490b5f7bfb9f776241
Mário Salvador <mariopaulobunga@gmail.com>
2025-01-17 01:43:52 +0100

    feat: aprimorar seção hero com suporte a temas claro e escuro e ajustes de estilo

--- src/components/landing/hero-section/index.tsx
+++ src/components/landing/hero-section/index.tsx
@@ -1,34 +1,44 @@
-import { ShimmerButton } from "@/components/ui/shimmer-button"
-import { ChevronRight } from "lucide-react"
-import { ReactIcon, TailwindCSSIcon, TypescriptIcon } from "../Icon"
-
+import { ShimmerButton } from "@/components/ui/shimmer-button";
+import { ChevronRight } from "lucide-react";
+import { ReactIcon, TailwindCSSIcon, TypescriptIcon } from "../Icon";
 
 export const HeroSection = () => {
-
   return (
-    <div className="w-full bg-gray-50 h-screen">
-      <section className="flex flex-col items-center gap-8 pt-44  h-full">
+    <div className="w-full bg-gray-50 dark:bg-black h-screen">
+      <section className="flex flex-col items-center gap-8 pt-44 h-full">
+        {/* Título */}
         <article className="flex flex-col items-center">
-          <h1 className="text-5xl font-bold animate-pulse">
+          <h1 className="text-5xl font-bold animate-pulse text-gray-900 dark:text-gray-100 ">
             UI Camera for
           </h1>
-          <h1 className="text-5xl font-bold  animate-pulse">
-            React Web Aplication
+          <h1 className="text-5xl font-bold animate-pulse text-gray-900 dark:text-gray-100">
+            React Web Application
           </h1>
         </article>
+
+        {/* Descrição */}
         <article className="flex flex-col items-center">
-          <p className="text-center text-md ">
-            Open source component built for <strong>React</strong>, <strong>Typescript</strong> and <strong>TailwindCSS</strong>
+          <p className="text-center text-md text-gray-700 dark:text-gray-300 ">
+            Open source component built for <strong>React</strong>,{" "}
+            <strong>Typescript</strong>, and <strong>TailwindCSS</strong>
           </p>
-          <p>Based in black and white theme</p>
+          <p className="text-gray-700 dark:text-gray-300">
+            Based in black and white theme
+          </p>
         </article>
-        <ShimmerButton className=" flex justify-between shadow-2xl px-2 py-3 rounded-[20px] w-max h-[40px] 	">
-          <span className=" flex w-full justify-between text-md items-center gap-5 whitespace-pre-wrap leading-none tracking-tight">
+
+        {/* Botão */}
+        <ShimmerButton className="flex justify-between shadow-2xl px-2 py-3 rounded-[20px] w-max h-[40px] bg-white dark:bg-gray-800 dark:text-gray-100">
+          <span className="flex w-full justify-between text-md items-center gap-5 whitespace-pre-wrap leading-none tracking-tight">
             Get started
-            <ChevronRight size={16} className="hover:animate-ping" />
+            <ChevronRight
+              size={16}
+              className="hover:animate-ping text-gray-700 dark:text-gray-300"
+            />
           </span>
         </ShimmerButton>
 
+        {/* Ícones */}
         <article className="flex gap-5">
           <ReactIcon size="30" />
           <TypescriptIcon size="30" />
@@ -36,5 +46,5 @@ export const HeroSection = () => {
         </article>
       </section>
     </div>
-  )
-}
+  );
+};
