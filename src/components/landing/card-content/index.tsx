fa4e96218983d6fe5dd393165d36a26529cf11d8
Mário Salvador <128391174+mariosalvador@users.noreply.github.com>
2025-01-17 18:41:53 +0000

    feat: update layout and styling for card and main content components

--- src/components/landing/card-content/index.tsx
+++ src/components/landing/card-content/index.tsx
@@ -1,7 +1,7 @@
 
 export const CardContent = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex w-full p-12 items-center justify-center rounded-md shadow-md border border-gray-200 bg-gray-50/50 dark:border-gray-700 dark:bg-black  ">
+    <div className="flex w-full p-5  md:p-12 items-center justify-center rounded-md shadow-md border border-gray-200 bg-gray-50/50 dark:border-gray-700 dark:bg-black  ">
       {children}
     </div>
   );
--- src/components/landing/main-content/index.tsx
+++ src/components/landing/main-content/index.tsx
@@ -9,10 +9,10 @@ import { DemoCameraFour } from "@/components/ui-camera/demo-example/demo-4";
 export const MainContent = () => {
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 space-y-14 bg-gray-50 dark:bg-black text-gray-900 dark:text-gray-100">
+    <main className="flex min-h-screen flex-col items-center justify-between  px-8 md:p-16 pb-2 space-y-14 bg-gray-50 dark:bg-black text-gray-900 dark:text-gray-100">
       {/* Seção de título e descrição */}
-      <section className="flex flex-col gap-2 items-center">
-        <h1 className="text-5xl font-bold">Component Demos</h1>
+      <section className="flex flex-col gap-2 pt-5 items-center text-center">
+        <h1 className="text-5xl font-bold text-center">Component Demos</h1>
         <p>
           Here are some of the UI Camera components that you can use in your
           projects.
@@ -20,8 +20,8 @@ export const MainContent = () => {
       </section>
 
       {/* Conteúdo principal */}
-      <div className="z-10 flex flex-col w-full items-center justify-between font-mono text-sm lg:flex">
-        <div className="fixed bottom-0 left-0 flex flex-col gap-10 h-48 w-full items-center justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+      <div className=" flex flex-col w-full items-center justify-between font-mono text-sm lg:flex ">
+        <div className=" flex flex-col gap-10 h-full w-full items-center justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black static  lg:bg-none">
 
           <CardContent>
             <DemoCameraOne/>
